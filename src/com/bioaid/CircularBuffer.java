67bff7f41d52758e3be7f16347d2e1261d3bf2b9
jaro1305 <ax59pro13>
2013-06-09 12:07:54 +0100

    changed CircularBuffer to LinkedList

--- src/com/bioaid/CircularBuffer.java
+++ src/com/bioaid/CircularBuffer.java
@@ -2,43 +2,70 @@ package com.bioaid;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 
 /**
  * Created by jaro on 6/8/13.
  */
 public class CircularBuffer<T> {
 
-    ArrayList<T> list = new ArrayList<T>();
+    LinkedList<T> list = new LinkedList<T>();
     int capacity;
-    int front = 0;
-    int back = 0;
-    boolean empty = true;
 
     public CircularBuffer(int size) {
         capacity = size;
-        for (int i = 0; i < capacity; i++) {
-            list.add(null);
-        }
     }
 
     public void set_capacity(int capacity) {
-        for (int i = this.capacity; i < capacity; i++) {
-            list.add(null);
-        }
         this.capacity = capacity;
     }
 
     public void push_back(T v) {
-        list.set(back, v);
-        back = (back + 1) % capacity;
-        if (back == front && empty) {
-            front = (front + 1) % capacity;
-        }
-        empty = false;
+        list.add(v);
+        if (list.size() > capacity)
+            list.remove(0);
     }
 
     public T front() {
-        return list.get(front);
+        return list.get(0);
+    }
+
+    public static void main(String[] args) {
+            CircularBuffer<Integer> cb = new CircularBuffer<Integer>(3);
+            //cb.set_capacity(5);
+            cb.push_back(1);
+            System.out.println("front after 1 " + cb.front());
+            cb.push_back(2);
+            System.out.println("front after 2 " + cb.front());
+            cb.push_back(3);
+            System.out.println("front after 3 " + cb.front());
+//            std::cout << "0 " << cb[0] << std::endl;
+//            std::cout << "1 " << cb[1] << std::endl;
+//            std::cout << "2 " << cb[2] << std::endl;
+
+            cb.push_back(4);
+            System.out.println("front after 4 " + cb.front());
+//            std::cout << "0 " << cb[0] << std::endl;
+//            std::cout << "1 " << cb[1] << std::endl;
+//            std::cout << "2 " << cb[2] << std::endl;
+
+            System.out.println(cb.front());
+            System.out.println(cb.front());
+
+            cb.push_back(3);
+            cb.push_back(4);
+            cb.push_back(5);
+            cb.push_back(6);
+            cb.push_back(7);
+
+            System.out.println(cb.front());
+            System.out.println(cb.front());
+
+            cb.push_back(8);
+
+            System.out.println(cb.front());
+            System.out.println(cb.front());
+
     }
 
 }
